b12dbb489b5d5825484c8bcd07d68480bdabcbfc
LeylaDiana <lola.leyla85@gmail.com>
2022-08-19 11:18:36 +0800

    add in user photo , add in User Details

--- app/src/main/java/com/example/mommyjourneyapp/UserDashboard.kt
+++ app/src/main/java/com/example/mommyjourneyapp/UserDashboard.kt
@@ -1,21 +1,25 @@
 package com.example.mommyjourneyapp
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.AdapterView
+
 import android.widget.ArrayAdapter
 import android.widget.ListView
+import androidx.cardview.widget.CardView
 
 class UserDashboard : AppCompatActivity() {
-    val fruits = arrayOf("Apple","Banana","PinaApple","Mango","Orange" )
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_dashboard)
-        val listView = findViewById<ListView>(R.id.list)
+        val arrayAdapter: ArrayAdapter<*>
+        val users = arrayOf( "Marsita Adlina Abdul Rahman", "590216085238", "16/02/1959", "0122136137" )
 
-
+        // access the listView from xml file
+        val mListView = findViewById<ListView>(R.id.userlist)
+        arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, users)
+        mListView.adapter = arrayAdapter
+        mListView.setBackgroundColor(Color.parseColor("#E7E0EC"));
 
     }
 }
