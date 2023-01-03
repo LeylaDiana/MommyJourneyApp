b921ed7173248200796bd0401f27246f07eb4887
LeylaDiana <lola.leyla85@gmail.com>
2023-01-03 12:51:25 +0800

    AddinCurrentAppointmentDate

--- app/src/main/java/com/example/mommyjourneyapp/CreateNewAppoinment.kt
+++ app/src/main/java/com/example/mommyjourneyapp/CreateNewAppoinment.kt
@@ -1,11 +1,25 @@
 package com.example.mommyjourneyapp
 
 import android.annotation.SuppressLint
+import android.app.Activity
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+
 import android.widget.Button
 import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
+
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.datepicker.MaterialDatePicker
 import com.google.firebase.auth.FirebaseAuth
@@ -13,10 +27,14 @@ import com.google.firebase.database.DatabaseReference
 import com.google.firebase.database.FirebaseDatabase
 import java.text.SimpleDateFormat
 import java.util.*
-
+import com.example.mommyjourneyapp.databinding.ActivityCreateNewAppoinmentBinding
 class CreateNewAppoinment : AppCompatActivity() {
 
 
+    private lateinit var binding: ActivityCreateNewAppoinmentBinding
+
+    private val CHANNEL_ID = "channel_id_example_01"
+    private val notificationId = 101
     private lateinit var dbref: DatabaseReference
     private lateinit var userRecyclerview: RecyclerView
     private lateinit var userArrayList: ArrayList<Users>
@@ -25,11 +43,59 @@ class CreateNewAppoinment : AppCompatActivity() {
     var databases: FirebaseDatabase? = null
     private var auth: FirebaseAuth? = null
     @SuppressLint("MissingInflatedId")
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_new_appoinment)
+
+        binding = ActivityCreateNewAppoinmentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        createNotificationChannel()
+
+        binding.btnNotify.setOnClickListener {
+            sendNotification()
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun createNotificationChannel()
+    {
+        val name = "Notification Title"
+        val descriptionText = "Notification Description"
+        val importance = NotificationManager.IMPORTANCE_DEFAULT
+        val channel = NotificationChannel(CHANNEL_ID,name,importance).apply {
+            description = descriptionText
+        }
+        val notificationManager: NotificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        notificationManager.createNotificationChannel(channel)
+    }
+
+    private fun sendNotification()
+    {
+        val intent = Intent(this,MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        }
+        val pendingIntent: PendingIntent = PendingIntent.getActivity(this,0,intent,0)
+        val bitmap : Bitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.newmom)
+        val bitmapLargeIcon : Bitmap = BitmapFactory.decodeResource(applicationContext.resources,R.drawable.user)
+
+        val builder = NotificationCompat.Builder(this,CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .setContentTitle("Example Title")
+            .setContentText("Example Description")
+            .setLargeIcon(bitmapLargeIcon)
+            .setStyle(NotificationCompat.BigPictureStyle().bigPicture(bitmap))
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+        with(NotificationManagerCompat.from(this)){
+            notify(notificationId,builder.build())
+        }
+
         val btnDatePicker = findViewById(R.id.CreateNewAppointment) as Button
-        btnDatePicker.setOnClickListener {
+
+    btnDatePicker.setOnClickListener {
 
             val datePicker = MaterialDatePicker.Builder.datePicker().build()
             datePicker.show(supportFragmentManager, "DatePicker")
